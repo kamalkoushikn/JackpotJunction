0d977dc2f95ea6da350b9082d7230b3eb266e211
kamalkoushikn <>
2024-12-01 23:28:59 -0800

    model class setup

---
+++ Contacts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import ContactListView
+urlpatterns = [
+    path("", ContactListView.as_view(), name="contact_list"),
+]
--- Contacts/views.py
+++ Contacts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Contact
 # Create your views here.
+class ContactListView(ListView):
+    model = Contact
